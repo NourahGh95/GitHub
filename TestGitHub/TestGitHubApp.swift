529ae15a6f7dd6e29a8ec511c5c0b22fad821401
Nourah <nourah.abghanim@hotmail.com>
2022-12-12 17:57:07 +0300

    Initial Commit

---
+++ TestGitHub/TestGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitHubApp.swift
+//  TestGitHub
+//
+//  Created by Nourah on 18/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct TestGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
